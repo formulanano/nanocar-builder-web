c3e80da604e74c23ee2df73350148fb7749c04e2
drac <seyhan.drc@gmail.com>
2021-09-27 00:07:48 +0300

    fix(FPSMonitor): fix function return

--- src/app/commons/FPSMonitor/FPSMonitor.jsx
+++ src/app/commons/FPSMonitor/FPSMonitor.jsx
@@ -6,8 +6,6 @@ import FPSStats from "react-fps-stats";
  *
  * @type {Function} renders FPS stats on the screen
  */
-const FPSMonitor = () => {
-  return <FPSStats top="auto" left="auto" bottom={20} right={20} />;
-};
+const FPSMonitor = () => <FPSStats top="auto" left="auto" bottom={20} right={20} />;
 
 export default FPSMonitor;
